e14de355b1403326aed9b873787c31938a598196
Noe Juan <officiallucidtraveler@gmail.com>
2026-01-17 01:44:07 -0800

    deleted prank entirely

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,1 @@
-const prankCard = document.getElementById("pranked");
 
-const roll = `<iframe width="1879" height="718" src="https://www.youtube.com/embed/2Pgl-JC2sgk" title="PINK GUY - セックス大好き" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
-
-function addPrank() {
-	console.log("Hello, world!");
-	prankCard.innerHTML = roll;
-	prankCard.getElementsByTagName("iframe")[0].requestFullscreen();
-
-	prankCard.removeEventListener("click", addPrank);
-}
-
-prankCard.addEventListener("click", addPrank);
